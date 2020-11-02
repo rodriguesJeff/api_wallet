58684184bbe2ba2d098796d62394424b92483d63
Jefferson Rodrigues <jeffersondavid179@gmail.com>
2020-11-01 23:39:28 -0300

    update the database

--- database/bd.js
+++ database/bd.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/wallet', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://admin:1234@cluster0.dppu7.gcp.mongodb.net/wallet?retryWrites=true&w=majority');
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
